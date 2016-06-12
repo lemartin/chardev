1bebbe3ba6ab4e711c0b7c5fb3786e1a272b956d
Martin Waßmann <lemartin@chardev.org>
2016-06-12 23:45:47 +0200

    Improved performance by replacing unordered_set in dehashing algorithm by array of vectors

--- tools/dehasher/src/Resolver.cpp
+++ tools/dehasher/src/Resolver.cpp
@@ -14,10 +14,28 @@ namespace {
         }
     };
 
+    struct UnknownHashes {
+        std::vector<uint64_t> buckets[1 << 16];
+
+        void add(uint64_t value) {
+            buckets[(uint16_t) (value >> 48)].push_back(value);
+        }
+
+        bool contain(uint64_t value) const {
+            for (uint64_t possibleMatch : buckets[(uint16_t) (value >> 48)]) {
+                if (possibleMatch == value) {
+                    return true;
+                }
+            }
+
+            return false;
+        };
+    };
+
     struct Resolver {
         const std::vector<char> &alphabet;
         const std::vector<const char *> &extensions;
-        const chardev::wowdb::Jenkins96Hashes &unknownHashes;
+        const UnknownHashes &unknownHashes;
         ResolvedAdder &adder;
         char buffer[1024];
         uint32_t hashLowDw;
@@ -31,7 +49,7 @@ namespace {
         for (char letter : alphabet) {
             buffer[length] = letter;
 
-            for (const char* extension: extensions) {
+            for (const char *extension: extensions) {
                 strcpy(&buffer[length + 1], extension);
 
                 hashHighDw = 0;
@@ -39,7 +57,7 @@ namespace {
 
                 hashlittle2(buffer, length + 5, &hashHighDw, &hashLowDw);
 
-                if (unknownHashes.find(((uint64_t) hashHighDw << 32) | hashLowDw) != unknownHashes.end()) {
+                if (unknownHashes.contain(((uint64_t) hashHighDw << 32) | hashLowDw)) {
                     adder(std::string(buffer, 0, length + 5));
                 }
             }
@@ -63,14 +81,19 @@ const std::vector<std::string> chardev::wowdb::HashResolver::resolve(const Jenki
 
     std::vector<boost::thread> threads;
 
-    for (int threadIndex = 0; threadIndex < threadCount; threadIndex++) {
-        boost::thread thread([this, &hashes, &prefix, &depth, &adder, threadIndex]() {
+    UnknownHashes unknownHashes;
+    for (uint64_t value : hashes) {
+        unknownHashes.add(value);
+    }
 
-            for(auto it = alphabet.begin() + threadIndex; it < alphabet.end(); it += threadCount) {
+    for (int threadIndex = 0; threadIndex < threadCount; threadIndex++) {
+        boost::thread thread([this, &unknownHashes, &prefix, &depth, &adder, threadIndex]() {
+
+            for (auto it = alphabet.begin() + threadIndex; it < alphabet.end(); it += threadCount) {
                 std::string threadPrefix(prefix);
                 threadPrefix.append(1, *it);
 
-                Resolver resolver = {alphabet, extensions, hashes, adder};
+                Resolver resolver = {alphabet, extensions, unknownHashes, adder};
                 strcpy(resolver.buffer, threadPrefix.c_str());
 
                 resolver.resolve(threadPrefix.length(), depth - 1);
